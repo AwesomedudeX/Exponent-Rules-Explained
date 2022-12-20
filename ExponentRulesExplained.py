dc9fd7f79326f8955af0a3d6c6f358691ca8dfcb
AwesomedudeX <102268445+AwesomedudeX@users.noreply.github.com>
2022-12-19 18:50:54 -0700

    Update ExponentRulesExplained.py

--- ExponentRulesExplained.py
+++ ExponentRulesExplained.py
@@ -10,4 +10,4 @@ st.markdown(f'<h1 style="color:#FF0000;font-size:40px;">{sect}</h1>', unsafe_all
 if sect == "The Basics":
 
 	st.markdown(f'<h2 style="color:#FFAA00;font-size:25px;">What is a Base?</h2>', unsafe_allow_html=True)
-	st.write("A base is the \"main number\" in an expression with only a number and an exponent. This number is used when applying the exponent, and nothing else. For example, if you have ${4^{3}}$, 4 would be the base, and the 3 would be the exponent (more on that later). The base doesn't have to be just a single number, though. It can also be an expression inside brackets")
+	st.write("A base is the \"main number\" in an expression with only a number and an exponent. This number is used when applying the exponent, and nothing else. For example, if you have ${4^{3}}$ as your exponential expression, 4 would be the base, and the 3 would be the exponent (more on that later). The base doesn't have to be just a single number, though. It can also be an expression inside brackets, like ${(10/2-1)^{3}}$, which would be the same thing")
