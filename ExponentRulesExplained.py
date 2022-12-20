df3d4f463f7fd1284f1bb643bcda7d0d4a8ed00b
AwesomedudeX <102268445+AwesomedudeX@users.noreply.github.com>
2022-12-19 18:38:08 -0700

    Update ExponentRulesExplained.py

--- ExponentRulesExplained.py
+++ ExponentRulesExplained.py
@@ -5,9 +5,9 @@ warnings.filterwarnings("ignore")
 
 sect = st.sidebar.selectbox("Navigate:", ("The Basics", "Fixed Rules", "Product, Quotient and Power Rules", "Raising a Product or Quotient to a Power"))
 
-st.header(sect)
+st.markdown(f'<h1 style="color:#FF0000;font-size:25px;">{sect}</h1>', unsafe_allow_html=True)
 
 if sect == "The Basics":
 
-	st.subheader("What is a Base?")
+	st.markdown(f'<h2 style="color:#FFFF00;font-size:25px;">What is a Base?</h2>', unsafe_allow_html=True)
 	st.write("A ")
